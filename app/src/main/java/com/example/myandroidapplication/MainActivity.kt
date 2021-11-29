3bb904856b482dc979423861b5e27fa9f945f6a7
Nikolay4819 <konar@LAPTOP-3ND34C4V>
2021-11-29 22:39:26 +0500

    выполненное ДЗ

--- app/src/main/java/com/example/myandroidapplication/MainActivity.kt
+++ app/src/main/java/com/example/myandroidapplication/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.myandroidapplication
 
+import android.content.ContentValues.TAG
+import android.util.Log
+
+
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+const val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG,"Ты видишь в зеркале своё изображенье?")
     }
+
+    override fun onStart () {
+        super.onStart()
+        Log.d(TAG,"Скажи ему: Пора подобное создать;")
+    }
+
+    override fun onResume () {
+        super.onResume()
+        Log.d(TAG,"Иначе у земли ты совершишь хищенье,")
+    }
+
+    override fun onPause (){
+        super.onPause()
+        Log.d (TAG, "У юной матери отнимешь благодать.")
+    }
+
+    override fun onStop (){
+         super.onStop()
+         Log.d (TAG, "Где та красавица, чья девственная нива")
+    }
+
+    override fun onDestroy () {
+          super.onDestroy()
+          Log.d (TAG, "Такого пахаря отвергла бы, как ты?")
+    }
+
 }
+
